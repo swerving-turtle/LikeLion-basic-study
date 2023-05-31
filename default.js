76bc349225a7bc8117c9d5029183ed5462841a0a
Keonwoo Moon <swervingturtle@gmail.com>
2023-05-31 09:48:39 +0900

    조건부 제어문

--- default.js
+++ default.js
@@ -111,7 +111,64 @@
 //result = num1 % num2;
 //document.write("the remainder is : " + result + lineBreak);
 
-var base = parseFloat(prompt("밑변 : "));
-var height = parseFloat(prompt("높이 : "));
-result = base * height * 0.5;
-document.write("삼각형의 넓이: " + result + "<br/>");
+//var base = parseFloat(prompt("밑변 : "));
+//var height = parseFloat(prompt("높이 : "));
+//result = base * height * 0.5;
+//document.write("삼각형의 넓이: " + result + "<br/>");
+
+//var cels = parseFloat(prompt("섭씨 입력: "));
+//var farn = cels * (9 / 5) + 32;
+//document.write("화씨: " + farn);
+
+//var num1 = 20;
+//var num2 = 10;
+//var num3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//
+//console.log('일반 크기비교')
+//console.log(num1 > num2, num1, '>', num2);
+//console.log(num1 >= num2, num1, '>=', num2);
+//console.log(num1 < num2, num1, '<', num2);
+//console.log(num1 <= num2, num1, '<=', num2);
+//console.log('같은지 여부')
+//console.log(num1 == num4, num1, '==', num4);
+//console.log(num1 != num4, num1, '!=', num4);
+//console.log('===')
+//console.log(num1 === num3, num1, '===', num3);
+//console.log(num2 === num3, num2, '===', num3);
+//console.log(num2 == num3, num2, '==', num3);
+//
+//console.log('num1 > num2 && num1 > num5: ', num1 > num2 && num1 < num5);
+//console.log('num1 > num2 || num1 > num5: ', num1 > num2 || num1 > num5);
+
+var num1 = parseInt(prompt('첫번째 숫자 입력 : '));
+var num2 = parseInt(prompt('두번째 숫자 입력 : '));
+
+if(num1 > num2) {
+    console.log("큰 수는 num1 : " + num1);
+}
+
+if(num1 < num2) {
+    console.log('큰 수는 num2 : ' + num2);
+}
+
+if(num1 == num2) {
+    console.log("같은 수");
+}
+
+if(num1 > num2) {
+    console.log("큰 수 num1:" + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2:" + num2);
+} else if (num1 == num2) {
+    console.log("같은 수");
+}
+
+if(num1 > num2) {
+    console.log("큰 수 num1:" + num1);
+} else if (num1 < num2) {
+    console.log("큰 수 num2:" + num2);
+} else  {
+    console.log("같은 수");
+}
