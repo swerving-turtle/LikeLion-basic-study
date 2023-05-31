90013e6a95d1a3543cdf9021cc63bd3206ff9f05
Keonwoo Moon <swervingturtle@gmail.com>
2023-05-31 15:00:43 +0900

    날짜 객체 사용 실습

--- default.js
+++ default.js
@@ -183,38 +183,59 @@
 //    console.log('Consonant');
 //}
 
-const digit = parseInt(prompt("숫자 입력: "));
-switch (digit){
-    case 0:
-        document.write('Zero');
-        break;
-    case 1:
-        document.write('One');
-        break;
-    case 2:
-        document.write('Two');
-        break;
-    case 3:
-        document.write('Three');
-        break;
-    case 4:
-        document.write('Four');
-        break;
-    case 5:
-        document.write('Five');
-        break;
-    case 6:
-        document.write('Six');
-        break;
-    case 7:
-        document.write('Seven');
-        break;
-    case 8:
-        document.write('Eight');
-        break;
-    case 9:
-        document.write('Nine');
-        break;
-    default:
-        document.write("Not a digit");
-}
+//const digit = parseInt(prompt("숫자 입력: "));
+//switch (digit){
+//    case 0:
+//        document.write('Zero');
+//        break;
+//    case 1:
+//        document.write('One');
+//        break;
+//    case 2:
+//        document.write('Two');
+//        break;
+//    case 3:
+//        document.write('Three');
+//        break;
+//    case 4:
+//        document.write('Four');
+//        break;
+//    case 5:
+//        document.write('Five');
+//        break;
+//    case 6:
+//        document.write('Six');
+//        break;
+//    case 7:
+//        document.write('Seven');
+//        break;
+//    case 8:
+//        document.write('Eight');
+//        break;
+//    case 9:
+//        document.write('Nine');
+//        break;
+//    default:
+//        document.write("Not a digit");
+//}
+
+var date = new Date();
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month)
+
+var currentDate = date.getDate();
+console.log(currentDate);
+
+var currentDay = date.getDay();
+console.log(currentDay);
+
+var currentHour = date.getHours();
+console.log(currentHour);
+
+var currentMinutes = date.getMinutes();
+console.log(currentMinutes);
