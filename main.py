45428ec96d1ee608c9cd8fd57d3a55bf10a9d4a8
Keonwoo Moon <swervingturtle@gmail.com>
2023-06-12 13:04:51 +0900

    패키지 실습

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
-class ParentClass:
-    def __init__(self):
-        self.name = 'parent'
-        self.number = 10
+class Engine:
+    def start(self):
+        return "Engine started"
+    def stop(self):
+        return "Engine stopped"
 
-    def __str__(self):
-        return f'ParentClass name: {self.name}, number: {self.number}'
+class Wheels:
+    def rotate(self):
+        return "Wheels are rotating"
 
-    def add_num(self, new_number):
-        print('부모: ', new_number, '만큼 더해야지')
-        self.number = self.number + new_number
-class ChildClass(ParentClass):
-    def __init__(self):
-        super().__init__()
-        self.name = 'child'
-    def __str__(self):
-        return f'ChildClass name: {self.name}, number: {self.number}'
+class Car(Engine, Wheels):
+    pass
 
-    def add_num(self, new_number):
-        print('말 안듣는 자식: 고정적으로 5 더할건데?')
-        self.number = self.number + 5
-parent = ParentClass()
-child = ChildClass()
+my_car = Car()
 
-print('7일 더하세요')
-parent.add_num(7)
-child.add_num(7)
-print(parent)
-print(child)
+print(my_car.start())
+print(my_car.rotate())
